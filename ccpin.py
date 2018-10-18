614796b4dd9a85c8bb9ae816b535831fd138260c
Brett Cloward <brettcloward@mail.weber.edu>
2018-10-18 20:39:41 +0000

    Program working

--- ccpin.py
+++ ccpin.py
@@ -1,44 +1,51 @@
 #!/usr/bin/env python3
 """
-This program checks to see if a users pin is valid or not
+This program checks to see if a users input pin is valid or not
 """
 
 
-PIN = "1234"
+RETURNS = []
 
 
-def valid_input(userpin):
+def valid_input(userpin, pin):
     """
-    checks for valid input
+    Checks if an input PIN Number is valid or invalid
     """
-    if userpin == PIN:
-        print("Your PIN is correct")
-        return True
-
+    # Check to see if userpin length is 4 characters long
     if len(userpin) != 4:
         print("Invalid PIN length. Correct format is: <9876>")
 
+    # Check to see if userpin input contains letters
     bad = False
-    for c in userpin:
-        if c.isdigit() == False:
+    for num in userpin:
+        if num.isdigit() is False:
             bad = True
             break
     if bad:
         print("Invalid PIN character. Correct format is: <9876>")
 
+    # Check to see if userpin is correct or incorrect
+    if userpin == pin:
+        print("Your PIN is correct")
+        return True
+    elif userpin != pin:
+        print("Your PIN is incorrect")
+        return False
+
+
 def main():
     """
     Test your module
     """
+    pin = "1234"
     userpin = input("Please enter your PIN: ")
-    r = valid_input(userpin)
-    timesincorrect = 0
-    if r == False:
-        timesincorrect += 1
-
-
-
-    #pass
+    returnvalue = valid_input(userpin, pin)
+    if returnvalue is False:
+        RETURNS.append(False)
+        if len(RETURNS) == 3:
+            print("Your account is locked")
+            exit(1)
+        main()
 
 
 if __name__ == "__main__":
