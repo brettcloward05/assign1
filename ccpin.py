16bdc1fdd375f99de40ec118f847e19f5cfd4f84
Brett Cloward <brettcloward@mail.weber.edu>
2018-10-24 16:31:37 +0000

    completed

--- ccpin.py
+++ ccpin.py
@@ -4,30 +4,25 @@ This program checks to see if a users input pin is valid or not
 """
 
 
-RETURNS = []
-
-
-def valid_input(userpin, pin):
+def valid_input(userpin):
     """
     Checks if an input PIN Number is valid or invalid
+    Args:
+        userpin: the userpin the user enters
+    Returns:
+        False or True depending on the userpin
     """
-    # Check to see if userpin length is 4 characters long
-    if len(userpin) != 4:
-        print("Invalid PIN length. Correct format is: <9876>")
+    pin = "1234"
 
-    # Check to see if userpin input contains letters
-    bad = False
-    for num in userpin:
-        if num.isdigit() is False:
-            bad = True
-            break
-    if bad:
-        print("Invalid PIN character. Correct format is: <9876>")
-
-    # Check to see if userpin is correct or incorrect
     if userpin == pin:
         print("Your PIN is correct")
         return True
+    elif len(userpin) != 4:
+        print("Invalid PIN length. Correct format is: <9876>")
+        return False
+    elif userpin.isdigit() is False:
+        print("Invalid PIN character. Correct format is: <9876>")
+        return False
     elif userpin != pin:
         print("Your PIN is incorrect")
         return False
@@ -35,17 +30,18 @@ def valid_input(userpin, pin):
 
 def main():
     """
-    Test your module
+    Tests the module
+    Checks invalid input up to 3 times
     """
-    pin = "1234"
-    userpin = input("Please enter your PIN: ")
-    returnvalue = valid_input(userpin, pin)
-    if returnvalue is False:
-        RETURNS.append(False)
-        if len(RETURNS) == 3:
-            print("Your account is locked")
-            exit(1)
-        main()
+    for i in range(3):
+        i += 1
+        userpin = input("Please enter your PIN: ")
+        returnvalue = valid_input(userpin)
+        if returnvalue is True:
+            exit(0)
+        if i == 3:
+            print("Your bank card is blocked")
+            exit(2)
 
 
 if __name__ == "__main__":
